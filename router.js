e872b6a774dbd82fae843bd04f8fc0dca0907e24
Rodrigo <infohotsmoke@gmail.com>
2022-04-25 10:42:51 +0200

    feat: router.js updated

--- router.js
+++ router.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const res = require('express/lib/response'); 
 
-const UsuarioRouter = require('./views/UsuarioRouter'); 
+const UsuariosRouter = require('./views/UsuariosRouter'); 
 const RecetasRouter = require('./views/RecetasRouter');
 const GuardadosRouter = require('./views/GuardadosRouter');
 
-router.use('/usuarios', UsuarioRouter); 
+router.use('/usuarios', UsuariosRouter); 
 router.use('/recetas', RecetasRouter); 
 router.use('/guardados', GuardadosRouter); 
 
