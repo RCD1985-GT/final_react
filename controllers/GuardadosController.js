fb3edf24e05010eb01687db796f075c5ef9024bb
Rodrigo <infohotsmoke@gmail.com>
2022-04-25 16:58:12 +0200

    feat:GuardadosController functions created

---
+++ controllers/GuardadosController.js
@@ -0,0 +1,42 @@
+
+const { Pedido } = require('../models/index'); // ¿PORQUE LLAMA A MODELS/INDEX.JS? // CAMBIAR
+
+
+const GuardadosController = {};
+
+
+// Funcion nuevoGuardado...
+
+GuardadosController.nuevoGuardado = (req,res) => {
+    
+    let body = req.body;
+
+    console.log("este es body",body)
+
+    Guardado.create({
+        recetaId: body.recetaId,
+        usuarioId: body.usuarioId,
+        
+    })
+    .then(guardado => {
+        if(guardado){
+            res.send(guardado)
+        }else{
+            res.send("No se ha guardado");
+        }
+    })
+    .catch((error => {
+        res.send(error)
+    }))
+}
+
+
+
+// Funcion totalGuardados....FUNCIONA
+GuardadosController.totalGuardados = (req, res) => {
+    Guardado.findAll()
+    .then(data => {
+        res.send(data)
+    });
+};
+module.exports = GuardadosController;
---
+++ views/GuardadosRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const auth = require("../middlewares/auth");
+const GuardadosController = require('../controllers/GuardadosController');
+
+// Nuevo guardado
+router.post('/nuevoGuardado', GuardadosController.nuevoGuardado);
+//http://localhost:3300/guardados/nuevoGuardado
+
+
+// // Total de pedidos
+// router.get('/totalPedidos', PedidosController.totalPedidos);
+// //http://localhost:3300/pedidos/totalPedidos
+
+module.exports = router;
