899d0e7490b3f49377276fbe33f4a02300f31b93
Rodrigo <infohotsmoke@gmail.com>
2022-04-25 10:40:31 +0200

    feat: router.js created

---
+++ router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const res = require('express/lib/response'); 
+
+const UsuarioRouter = require('./views/UsuarioRouter'); 
+const RecetasRouter = require('./views/RecetasRouter');
+const GuardadosRouter = require('./views/GuardadosRouter');
+
+router.use('/usuarios', UsuarioRouter); 
+router.use('/recetas', RecetasRouter); 
+router.use('/guardados', GuardadosRouter); 
+
+module.exports = router;
